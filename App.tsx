5d78a0a60bd249bca21e402666213887f5e20ce4
Natanael Razzotto <natanael8447@outlook.com>
2025-08-18 21:00:26 -0300

    feat: :tada: initialize React Native tic-tac-toe project

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import HomeScreens from './src/screens/HomeScreens'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <HomeScreens></HomeScreens>
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/screens/HomeScreens.tsx
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+export default function HomeScreen(){
+    return (
+        <View>
+            <Text >
+                Home works!!!
+            </Text>
+        </View>
+    )
+}
